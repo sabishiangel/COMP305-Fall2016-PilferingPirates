df7045505d450b1c01ef7d87da8b051681120383
sabishiangel <ashley.tjonhing@gmail.com>
2016-09-27 21:38:52 -0400

    added background

--- Assets/_Scripts/BackgroundController.cs
+++ Assets/_Scripts/BackgroundController.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BackgroundController : MonoBehaviour {
+/*Author: Ashley Tjon-Hing
+Date: September 26th 2016*/
 
-	// Use this for initialization
-	void Start () {
-	
+public class BackgroundController : MonoBehaviour {
+    //Private Instance Variables
+    private int _speed = 3;
+    private Transform _transform;
+
+    //Public Properties
+    public int Speed {
+        get { return this._speed; }
+        set { this._speed = value; }
+    } //shortcut public+tab+tab
+                                       // Use this for initialization
+    void Start () {
+        this._transform = this.GetComponent<Transform>();//storing transformations from object into '_transform'; keeps track of position in every frame
+        this._speed = 3; //sets background drop rate to _px per frame
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        this._Move();
+        this._BoundaryCheck();
 	}
+
+    private void _Move()
+    {
+        Vector2 newPosition = this.transform.position; //Old position and new position are equal; must use vector(can be temporary) to modify transform components
+        newPosition.y -= this._speed; //brings the image downwards every fram
+        this._transform.position = newPosition; //resets so that it can continue bringing the image down
+    }
+
+    //The following method checks if the top of the game object meets the top border of the screen
+    private void _BoundaryCheck()
+    {
+        if(this._transform.position.y <=-780f)
+        { this._reset(); }
+    }
+
+    //resets object to original position
+    private void _reset()
+    {
+        this._transform.position = new Vector2(0f, 780f); //using new object to shift image back
+    }
 }
